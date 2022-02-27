06d4bb3bdf303f6ef88db990007012ac35a7ef6d
walchko <walchko@users.noreply.github.com>
2022-02-26 18:59:32 -0700

    up

--- python/yivo/sensors.py
+++ python/yivo/sensors.py
@@ -15,6 +15,8 @@ MagneticField = namedtuple("MagneticField","mx my mz")
 Range = namedtuple("Range", "range")
 LaserScan = namedtuple("LaserScan", "ranges intensities")
 TemperaturePressure = namedtuple("TemperaturePressure", "temperature pressure")
+# Image = namedtuple("Image","rows cols depth frame")
+Image = namedtuple("Image","frame")
 
 
 SensorData = namedtuple("SensorData","header size unpack cls name")
@@ -29,10 +31,15 @@ LPS22 = SensorData(0xB2, 4*2+1, Struct('<2f').unpack, TemperaturePressure, "LPS2
 DPS310 = SensorData(0xB1, 4*2+1, Struct('<2f').unpack, TemperaturePressure, "DPS310")
 
 # 0xC0
+IR = SensorData(0xC1, 5, Struct('<f').unpack, Range,"IR")
 TFMINI = SensorData(0xF7, 5, Struct('<f').unpack, Range,"TFmini")
 
+# FIXME: this size is way greater than 1 byte: 256 < 4*768 ... could make 2 bytes (65536)
+# WARNING: driver has a LOT of floating point operations in it ... BEARE on SW float ops CPU
+MLX90640 = SensorData(0xDD, 4*768+1, Struct('<768f').unpack, Image,"MLX90640")
+
 SOX = SensorData(0xD1, 4*(3+3+1)+1, Struct('<7f').unpack, ImuAGT,"LSM6DSOX")
-S33 = SensorData(0xD1, 4*(3+3+1)+1, Struct('<7f').unpack, ImuAGT,"LSM6DS33")
+S33 = SensorData(0xD0, 4*(3+3+1)+1, Struct('<7f').unpack, ImuAGT,"LSM6DS33")
 
 SOXLIS = SensorData(0xF1, 4*(3+3+3+1)+1, Struct('<10f').unpack, ImuAGMT,"LSM6DSOXLIS")
 S33LIS = SensorData(0xF0, 4*(3+3+3+1)+1, Struct('<10f').unpack, ImuAGMT,"LSM6DS33LIS")
@@ -46,5 +53,10 @@ sensorList = [
     SOXLIS,
     S33LIS,
     SOX,
-    S33
+    S33,
+    MLX90640
 ]
+
+sensorDict = {}
+for s in sensorList:
+    sensorDict[s.header] = s
--- python/yivo/serialport.py
+++ python/yivo/serialport.py
@@ -6,8 +6,8 @@
 import time
 from . import files
 # from struct import Struct
-# from collections import namedtuple
-from .sensors import sensorList
+from collections import deque
+from .sensors import sensorDict
 import serial
 
 # # [0xFF,0xFF] # start
@@ -28,7 +28,7 @@ import serial
 
 class SerialPort:
     def __init__(self, port, buad=1000000):
-        self.serial = serial.Serial(port, buad, timeout=0.001)
+        self.serial = serial.Serial(port, buad, timeout=0.01) # to=0.001
         if not self.serial.is_open:
             print("*** serial fail ***")
             exit(1)
@@ -41,28 +41,39 @@ class SerialPort:
 
         if c is None:
             c = b"g\n"
-        elif c[-1] != b"\n":
-            c += b"\n"
+        # elif c[-1] != b"\n":
+        #     c += b"\n"
 
+        # print(f">> {c}")
         ser.write(c)
 
         # ff,ff,msglen
         while ser.in_waiting < 3:
             time.sleep(0.0001)
 
+        rbuf = deque(maxlen=2)
+        rbuf.append(ser.read(1))
+        header = deque([b'\xff',b'\xff'])
+
         while True:
             # get start header [0xff,0xff]
-            chr = ser.read(1)
-            if chr != b'\xff':
-                continue
-
-            chr = ser.read(1)
-            if chr != b'\xff':
+            rbuf.append(ser.read(1))
+            # chr = ser.read(1)
+            # rbuf.append(chr)
+            # if chr != b'\xff':
+            #     continue
+            #
+            # chr = ser.read(1)
+            # if chr != b'\xff':
+            #     continue
+            if rbuf != header:
                 continue
 
             try:
-                len = ser.read(1)
-                len = ord(len)
+                # print(">> reading now")
+                length = ser.read(1)
+                length = ord(length)
+                # print(f">> len: {length}")
             except Exception:
                 # time.sleep(0.002)
                 continue
@@ -72,28 +83,60 @@ class SerialPort:
         ans = {}
 
         # [0xff,0xff,len, [header, size, data, ...],[...],0xee,0xee]
-        while len > 5:
+        while length > 5:
             id = ser.read(1)
             id = ord(id)
+            # print(f">> ID: {hex(id)}")
 
             # sensors = (LIDAR, SOXLIS, DPS310,)
-            found = False
+            # found = False
 
-            for s in sensorList:
-                if id == s.header:
-                    data = ser.read(s.size - 1) # ser.read(4)
-                    len -= s.size # 5
+            # for s in sensorList: # better way???
+            #     if id == s.header:
+            #         data = ser.read(s.size - 1) # ser.read(4)
+            #         len -= s.size # 5
+            #         msg = s.cls._make(s.unpack(data))
+            #         # ans[type(msg).__name__] = msg
+            #         ans[s.name] = msg
+            #         found = True
+            #         break
+            #
+            # if not found:
+            #     print("wtf:", id, len)
+            #     exit(1)
+            try:
+                s = sensorDict[id]
+                # cnt = 0
+                data = b''
+                # while cnt < s.size-1:
+                #     d = ser.read(s.size - cnt - 1) # ser.read(4)
+                #     # print(f">> d: {d}")
+                #     data += d
+                #     cnt += len(d)
+                #     print(f">> data size: {cnt} should be: {s.size}")
+                data = ser.read(s.size - 1)
+
+                length -= s.size # 5
+                # print(data)
+                # print(f">> {len(data)}")
+                if id in [0xDD]:
+                    dd = [s.unpack(data)]
+                    msg = s.cls._make(dd)
+                else:
                     msg = s.cls._make(s.unpack(data))
-                    # ans[type(msg).__name__] = msg
-                    ans[s.name] = msg
-                    found = True
-                    break
-
-            if not found:
-                print("wtf:", id, len)
+                # ans[type(msg).__name__] = msg
+                ans[s.name] = msg
+            except KeyError:
+                print("wtf:", id, length)
                 exit(1)
 
         ans["timestamp"] = time.time()
+
+        ending = ser.read(2)
+        if ending != [0xee,0xee]:
+            ans = None
+
+        # print(ending)
         return ans
 
 
